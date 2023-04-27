90071197ef5e08307ef27257b3dd8eb8a9ed957d
qyz3112746 <qyz3112746@gmail.com>
2023-04-27 23:50:22 +0800

    035 Net Role

--- Source/Blaster/Character/BlasterCharacter.h
+++ Source/Blaster/Character/BlasterCharacter.h
@@ -30,6 +30,10 @@ private:
 
 	UPROPERTY(VisibleAnywhere, Category = Camera)
 	class UCameraComponent* FollowCamera;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class UWidgetComponent* OverheadWidget;
+
 public:	
 
 };
