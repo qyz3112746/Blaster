5f5f1d4fdd7453735b8076d55e860f07aa505faf
qyz3112746 <qyz3112746@gmail.com>
2023-05-04 16:48:09 +0800

    Correcting the Weapon Rotation

--- Source/Blaster/Character/BlasterAnimInstance.h
+++ Source/Blaster/Character/BlasterAnimInstance.h
@@ -60,6 +60,12 @@ private:
 	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
 	ETurningInPlace TurningInPlace;
 
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	FRotator RightHandRotation;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bLocallyControlled;
+
 	FRotator CharacterRotationLastFrame;
 	FRotator CharacterRotation;
 	FRotator DeltaRotation;
